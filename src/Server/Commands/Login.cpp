474d92ead3742c59b6737072e7dbab06d10ff253
savalet <savinien.petitjean@gmail.com>
2026-04-04 23:39:21 +0200

    Login can now create a new user if the enterred user does not exist

--- src/Server/Commands/Login.cpp
+++ src/Server/Commands/Login.cpp
@@ -1,4 +1,13 @@
+#include <cstring>
 #include <stdexcept>
+
+#ifdef DARWIN_KERNEL
+#include <sys/syslimits.h>
+#include <uuid/uuid.h>
+#else
+#include <limits.h>
+#include <uuid/uuid.h>
+#endif
 
 #include "Login.hpp"
 #include "Server/Client.hpp"
@@ -8,13 +17,15 @@
 namespace commands {
 void Login::execute(std::shared_ptr<Client> client,
                     std::vector<std::string> &args) {
-  if (!args.size() || args.size() > 1)
-    throw std::runtime_error("400 Bad request, no args or too much args");
+  if (!args.size() || args.size() > 1) {
+    client->sendMessage("400 Bad request, no args or too much args\n");
+    return;
+  }
 
-  auto users = client->getServer().get().getDatabase().getUsers();
+  auto &users = client->getServer().get().getDatabase().getUsers();
 
   for (auto &user : users) {
-    if (user.name == args[0]) {
+    if (std::string(user.name) == args[0]) {
       client->setLogged(true);
       client->setActualUser(user);
       LOG_DEBUG("User " + std::string(user.name) + " is logged in, uuid " +
@@ -25,6 +36,22 @@ void Login::execute(std::shared_ptr<Client> client,
     }
   }
 
-  client->sendMessage("404 User not found\n");
+  User newUser;
+  uuid_t raw_uuid;
+  uuid_generate(raw_uuid);
+  uuid_unparse_lower(raw_uuid, newUser.uuid);
+  std::strncpy(newUser.name, args[0].c_str(), MAX_NAME_LENGTH - 1);
+  newUser.name[MAX_NAME_LENGTH - 1] = '\0';
+
+  users.push_back(newUser);
+
+  client->setLogged(true);
+  client->setActualUser(newUser);
+
+  LOG_DEBUG("Created new user " + std::string(newUser.name) + " with uuid " +
+            std::string(newUser.uuid));
+  client->sendMessage(
+      "201 User created and logged in: " + std::string(newUser.name) +
+      " uuid " + std::string(newUser.uuid) + "\n");
 }
 } // namespace commands

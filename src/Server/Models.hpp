e561aa54cf0b2eb5d542741f2f283a04c1fb161e
savalet <savinien.petitjean@gmail.com>
2026-04-04 23:09:04 +0200

    Change models UUID_STR_LEN name to match the other macros name

--- src/Server/Models.hpp
+++ src/Server/Models.hpp
@@ -6,56 +6,56 @@
 constexpr std::size_t MAX_NAME_LENGTH = 32;
 constexpr std::size_t MAX_DESCRIPTION_LENGTH = 255;
 constexpr std::size_t MAX_BODY_LENGTH = 512;
-constexpr std::size_t UUID_STR_LEN = 37;
+constexpr std::size_t MAX_UUID_LENGTH = 37;
 
 #pragma pack(push, 1)
 
 struct User {
-  char uuid[UUID_STR_LEN];
+  char uuid[MAX_UUID_LENGTH];
   char name[MAX_NAME_LENGTH];
 };
 
 struct Team {
-  char uuid[UUID_STR_LEN];
+  char uuid[MAX_UUID_LENGTH];
   char name[MAX_NAME_LENGTH];
   char description[MAX_DESCRIPTION_LENGTH];
 };
 
 struct Channel {
-  char uuid[UUID_STR_LEN];
-  char team_uuid[UUID_STR_LEN];
+  char uuid[MAX_UUID_LENGTH];
+  char team_uuid[MAX_UUID_LENGTH];
   char name[MAX_NAME_LENGTH];
   char description[MAX_DESCRIPTION_LENGTH];
 };
 
 struct Thread {
-  char uuid[UUID_STR_LEN];
-  char channel_uuid[UUID_STR_LEN];
-  char creator_uuid[UUID_STR_LEN];
+  char uuid[MAX_UUID_LENGTH];
+  char channel_uuid[MAX_UUID_LENGTH];
+  char creator_uuid[MAX_UUID_LENGTH];
   char title[MAX_NAME_LENGTH];
   char body[MAX_BODY_LENGTH];
   uint64_t timestamp;
 };
 
 struct Reply {
-  char uuid[UUID_STR_LEN];
-  char thread_uuid[UUID_STR_LEN];
-  char creator_uuid[UUID_STR_LEN];
+  char uuid[MAX_UUID_LENGTH];
+  char thread_uuid[MAX_UUID_LENGTH];
+  char creator_uuid[MAX_UUID_LENGTH];
   char body[MAX_BODY_LENGTH];
   uint64_t timestamp;
 };
 
 struct Message {
-  char uuid[UUID_STR_LEN];
-  char sender_uuid[UUID_STR_LEN];
-  char receiver_uuid[UUID_STR_LEN];
+  char uuid[MAX_UUID_LENGTH];
+  char sender_uuid[MAX_UUID_LENGTH];
+  char receiver_uuid[MAX_UUID_LENGTH];
   char body[MAX_BODY_LENGTH];
   uint64_t timestamp;
 };
 
 struct Subscription {
-  char user_uuid[UUID_STR_LEN];
-  char team_uuid[UUID_STR_LEN];
+  char user_uuid[MAX_UUID_LENGTH];
+  char team_uuid[MAX_UUID_LENGTH];
 };
 
 #pragma pack(pop)

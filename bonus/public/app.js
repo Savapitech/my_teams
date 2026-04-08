185da6cbff8200def14dae10502354a6cbfbf540
titouanhct <titouan.hochet@epitech.eu>
2026-04-08 11:28:20 +0200

    Add first version of the login screen

--- bonus/public/app.js
+++ bonus/public/app.js
@@ -2,6 +2,47 @@ const ws = new WebSocket(`ws://${window.location.host}`);
 const log = document.getElementById('log');
 const input = document.getElementById('cmdInput');
 const sendBtn = document.getElementById('sendBtn');
+const username = sessionStorage.getItem('username');
+
+if (username) {
+    showappscreen(username);
+} else {
+    showloginscreen();
+}
+
+function showappscreen(username) {
+    document.getElementById('loginScreen').style.display = 'none';
+    document.getElementById('appScreen').style.display = 'flex';
+    //document.getElementById('userName').textContent = username;
+}
+
+function showloginscreen(username) {
+    document.getElementById('appScreen').style.display = 'none';
+    document.getElementById('loginScreen').style.display = 'flex';
+}
+
+let tryingLogin = false;
+
+function Login() {
+    const username = document.getElementById('usernameInput').value.trim();
+    if (!username) 
+        return;
+    tryingLogin = true;
+    ws.send(`LOGIN "${username}"`);
+}
+
+document.getElementById('loginBtn').addEventListener('click', Login);
+document.getElementById('usernameInput').addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') Login();
+});
+
+function logout() {
+    sessionStorage.removeItem('username');
+    ws.send('LOGOUT');
+    showloginscreen();
+}
+
+document.getElementById('logoutBtn').addEventListener('click', logout);
 
 function appendLog(text, type) {
     const div = document.createElement('div');
@@ -13,10 +54,24 @@ function appendLog(text, type) {
 
 ws.onopen = () => {
     appendLog('Connected to WebSocket proxy.', 'system');
+    if (username) {
+        ws.send(`LOGIN "${username}"`);
+    }
 };
 
 ws.onmessage = (event) => {
     const data = JSON.parse(event.data);
+    if (tryingLogin) {
+        tryingLogin = false;
+        if (data.message.startsWith('200')) {
+            const username = document.getElementById('usernameInput').value.trim();
+            sessionStorage.setItem('username', username);
+            showappscreen(username);
+        } else {
+            document.getElementById('login-error').style.display = 'block';
+        }
+        return;
+    }
     appendLog(data.message, data.type);
 };
 

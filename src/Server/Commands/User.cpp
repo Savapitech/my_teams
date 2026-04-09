ac5a202820c412d80675df1b3a5ca3f170a092f4
Arthur-gtr <arthur.gauthier@epitech.eu>
2026-04-09 13:11:24 +0200

    fix the User command on the server

--- src/Server/Commands/User.cpp
+++ src/Server/Commands/User.cpp
@@ -33,8 +33,8 @@ void UserCommand::execute(std::shared_ptr<Client> client,
       }
 
       client->sendMessage("200 User info: Name: " + std::string(user.name) +
-                          ", UUID: " + std::string(user.uuid) +
-                          ", Status: " + (isConnected ? "1" : "0") + "\n");
+                          " ,UUID: " + std::string(user.uuid) +
+                          " ,Status: " + (isConnected ? "1" : "0") + "\n");
 
       LOG_DEBUG("User info requested for UUID [" + targetUuid +
                 "] - Status: " + std::to_string(isConnected));
